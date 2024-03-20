49b7aa2ace9283fc5fa7e7704b291c7252c7c3fb
Luan Henrique <luanhcosta03@gmail.com>
2024-03-20 13:22:40 -0300

    Estilização do ícone de modo da tela

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Header from '../components/Header';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 export default function Home(){
 
     const [ escuro, setEscuro ] = React.useState('false');
-
+    
     const tema = {
         backgroundColor: escuro ? '#111111' : '#CFD8DC',
-        color: escuro ? '#0aff9d' : '#9575CD' 
+        color: escuro ? '#0aff9d' : '#9575CD', 
     }
 
     function mudarTema(){
@@ -17,7 +19,7 @@ export default function Home(){
 
     return(
         <Grid style={tema}>
-            <button onClick={mudarTema}>Tema</button>
+            <button onClick={mudarTema}>{escuro === true ? <LightModeIcon /> : <DarkModeIcon />}</button>
             <Header />
         </Grid>
     );
