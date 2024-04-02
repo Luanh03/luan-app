20d67200569fabd7afb7b221c44420fc42635b07
Luan Henrique <luanhcosta03@gmail.com>
2024-04-02 19:01:56 -0300

    Atualização em components e criação de Professional, Skills e Contacts

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,8 +3,11 @@ import Grid from '@mui/material/Grid';
 import Header from '../components/Header';
 import Bio from '../components/Bio';
 import Experience from '../components/Experience';
+import Professional from '../components/Professional';
+import Skills from '../components/Skills';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+
 
 export default function Home(){
 
@@ -12,7 +15,7 @@ export default function Home(){
     
     const tema = {
         backgroundColor: escuro ? '#CFD8DC' : '#111111',
-        color: escuro ?  '#9575CD' : '#0aff9d', 
+        color: escuro ?  '#9575CD' : '#9575CD', 
     }
 
     function mudarTema(){
@@ -21,10 +24,23 @@ export default function Home(){
 
     return(
         <Grid className="Main"style={tema}>
-            <button onClick={mudarTema}>{escuro === true ? <LightModeIcon /> : <DarkModeIcon />}</button>
+            <Grid className="GridNavBar">
+                <button className="ButtonNavBar" onClick={mudarTema}>{escuro === true ? <LightModeIcon /> : <DarkModeIcon />}</button>
+                <a  className="LogoNavBar" href="/">LUAN</a>
+                <ul className="ListNavBar">
+                    <li className="ItemNavBar"><a href="/">Início</a></li>
+                    <li className="ItemNavBar"><a href="/">Sobre</a></li>
+                    <li className="ItemNavBar"><a href="/">Projetos</a></li>
+                    <li className="ItemNavBar"><a href="/">Carreira Profissional</a></li>
+                    <li className="ItemNavBar"><a href="/">Habilidades</a></li>
+                    <li className="ItemNavBar"><a href="/">Contatos</a></li>
+                </ul>
+            </Grid>
             <Header />
             <Bio />
             <Experience />
+            <Professional />
+            <Skills />
         </Grid>
     );
 }
