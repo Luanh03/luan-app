cf6ab87453565358dc285e5dc5717ead89cd09c6
Luan Henrique <luanhcosta03@gmail.com>
2024-04-05 14:47:39 -0300

    Correções em tags Contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
-import Github from '../images/Github.svg';
-import Linkedin from '../images/Linkedin.svg'
+
 
 export default function Contacts(){
 
     return(
         <Grid className="GridContacts">
         <h1 className="TitleContacts">ENTRE EM CONTATO COMIGO!</h1>
-        <ul className="ListContacts">
-            <li><img className="ImgContacts" src={Github} /></li>
-            <li><img className="ImgContacts" src={Linkedin}/></li>
-        </ul>
+        <section>
+            <ul className="ListContacts">
+                <li><a className="ImgContacts"/></li>
+                <li><a className="ImgContacts"/></li>
+            </ul>
+        </section>
     </Grid>
     );
 }
